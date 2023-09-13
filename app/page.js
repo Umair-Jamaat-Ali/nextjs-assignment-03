64e959f9ef7aca677f61336582fe7b35d337e749
UMAIR JAMAAT ALI <umairansari758@gmail.com>
2023-09-13 13:52:20 +0500

    follwer button

--- app/page.js
+++ app/page.js
@@ -1,37 +1,73 @@
 'use client';
-import {useState} from 'react';
+import { useState } from 'react';
+import axios from "axios";
+
 export default function Home() {
 
-  const []
-  const [data , setData] = useState ([]);
- 
+  const [userName, setUserName] = useState(null);
+  const [data, setData] = useState("");
+  const [followers, setFollowers] = useState([]);
+
   const onLoadHandler = async () => {
-    let response = await fetch ("https://api.github.com/users/naveed-rana/followers");
+    setFollowers([]);
+    let response = await fetch(`https://api.github.com/users/${userName}`);
     response = await response.json();
     setData(response);
   }
-  return (
-   <>
-    <label> Enter User Name</label>
-    <input type="text" onChange={()=>} placeholder='Enter User Name' /> 
-    <button onClick={onLoadHandler}>Load API</button>
 
-    <ul>
-      {data.map((item , i) => {
-       return (
-       <li key="i">
-        {item.id}
-        <br/>
-        {item.login}
-        <br/>
-        {item.followers_url}
-        <br/>
-       <img src={item.avatar_url} style={{width: "50px"}} alt="" /> 
-        </li>
-        
-       )
-      })}
-    </ul>
-   </>
+  // const onFollowerHandler = async () => {
+  //   let response = await fetch(data.followers_url);
+  //   // response = await response.json();
+  //   console.log("response", response.data);
+  //   setFollowers(response.data);
+
+  // }
+
+  const onFollowerHandler = async () =>{
+    let response = await axios.get (data.followers_url);
+    console.log("response", response);
+    setFollowers(response.data);
+  }
+
+  return (
+    <>
+      <label> Enter User Name</label>
+      <input type="text" onChange={(e) => setUserName(e.target.value)} placeholder='Enter User Name' />
+      <button onClick={onLoadHandler}>Load API</button>
+
+      {data &&
+        <>
+          <h1>Github user</h1>
+          <img src={data.avatar_url} width={100} alt="" />
+          <span>bio: {data.bio} - {data.followers}</span>
+          <button onClick={onFollowerHandler}>Get followers</button>
+        </>
+      }
+
+      {followers.length >= 1 && (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {followers.map((element) => (
+              <tr key={element.id}>
+                <td>{element.id}</td>
+                <td>
+                  <img src={element.avatar_url} className='w-[50px] ' alt="" />
+                </td>
+                <td>{element.login}</td>
+                <td>{element.type}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   )
 }
